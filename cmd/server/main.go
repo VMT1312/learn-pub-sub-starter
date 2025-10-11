9b310deba1a449646a9ec6244d19281639f606b5
VMT1312 <thiemmavinh123@gmail.com>
2025-10-11 15:33:10 +0800

    Add RabbitMQ connection setup and update dependencies

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const connectionString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	}
+
+	defer conn.Close()
+
+	fmt.Println("Connected to RabbitMQ successfully")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
