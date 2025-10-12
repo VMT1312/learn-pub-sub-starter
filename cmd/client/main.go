5ad590f76c2ffa78900e0962079e90668ca28668
VMT1312 <thiemmavinh123@gmail.com>
2025-10-12 14:57:15 +0800

    Remove unused signal handling and add in command processing for client side

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -34,7 +32,40 @@ func main() {
 		log.Fatalf("Failed to declare and bind pause queue: %v", err)
 	}
 
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	game_state := gamelogic.NewGameState(username)
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "spawn":
+			fmt.Println("spawning...")
+			err := game_state.CommandSpawn(words)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				continue
+			}
+		case "move":
+			fmt.Println("moving...")
+			_, err := game_state.CommandMove(words)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				continue
+			}
+		case "status":
+			game_state.CommandStatus()
+		case "help":
+			gamelogic.PrintClientHelp()
+		case "spam":
+			fmt.Println("Spamming not allowed yet!")
+		case "quit":
+			gamelogic.PrintQuit()
+			return
+		default:
+			fmt.Printf("unknown command: %s\n", words[0])
+		}
+	}
 }
